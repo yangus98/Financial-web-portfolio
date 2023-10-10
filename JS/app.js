6099d3829c55f11649fae2bfef752faf90e70307
Giuseppe Bianconi <giuseppebianconi2@gmail.com>
2023-10-10 11:08:20 +0200

    spazi, remake esperienze lavorative

--- JS/app.js
+++ JS/app.js
@@ -27,4 +27,54 @@ function prec(){
 
 setInterval(succ, 10000);
 
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleBtn = document.getElementById('toggleBtn');
+
+    var toggleDivs = document.querySelectorAll('.toggleDiv');
+
+    toggleBtn.addEventListener('click', function () {
+      toggleDivs.forEach(function (div) {
+        div.classList.toggle('visible');
+      });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleBtn2 = document.getElementById('toggleBtn2');
+
+    var toggleDivs2 = document.querySelectorAll('.toggleDiv2');
+
+    toggleBtn2.addEventListener('click', function () {
+      toggleDivs2.forEach(function (div) {
+        div.classList.toggle('visible');
+      });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleBtn3 = document.getElementById('toggleBtn3');
+
+    var toggleDivs3 = document.querySelectorAll('.toggleDiv3');
+
+    toggleBtn3.addEventListener('click', function () {
+      toggleDivs3.forEach(function (div) {
+        div.classList.toggle('visible');
+      });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleBtn4 = document.getElementById('toggleBtn4');
+
+    var toggleDivs4 = document.querySelectorAll('.toggleDiv4');
+
+    toggleBtn4.addEventListener('click', function () {
+      toggleDivs4.forEach(function (div) {
+        div.classList.toggle('visible');
+      });
+    });
+});
+
+
+
 
